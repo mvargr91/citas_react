35513fb2a84326eebb7106a3c2dbefdfc666cfe6
mvargr <maurovarg1@gmail.com>
2022-04-30 16:31:49 -0500

    cambio en el headers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ function Header(){
     return (
         <>
             <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">Seguimiento Pacientes 
-                {" "}<span className="text-indigo-600">Veterinario</span>
+                {" "} con githib {''}
+                <span className="text-indigo-600">Veterinario</span>
             </h1>
         </>
     )
